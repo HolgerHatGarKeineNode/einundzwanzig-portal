c5a589200763997685ad8bc1454a07039196f4b2
Shift <shift@laravelshift.com>
2025-09-01 22:56:50 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,44 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->validateCsrfTokens(except: [
+            'horizon/*',
+            'auth/auth47-callback',
+        ]);
+
+        $middleware->web(\App\Http\Middleware\SetTimezoneMiddleware::class);
+
+        $middleware->throttleApi();
+        $middleware->api(\App\Http\Middleware\SetTimezoneMiddleware::class);
+
+        $middleware->group('nova-web', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\SetTimezoneForNovaMiddleware::class,
+        ]);
+
+        $middleware->group('translation', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\SetTimezoneMiddleware::class,
+            'can:translate',
+        ]);
+
+        $middleware->alias([
+            'needMeetup' => \App\Http\Middleware\NeedMeetupMiddleware::class,
+            'verified' => \App\Http\Middleware\CustomEnsureEmailVerified::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
