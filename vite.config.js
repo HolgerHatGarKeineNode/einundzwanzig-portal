35fba672b7649e2d0223c422663745d5c2438aca
user <fsociety.mkv@pm.me>
2025-09-02 03:09:39 +0200

    🧹 Remove `vite-plugin-static-copy` from `vite.config.js` as it is no longer used.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     server: {
@@ -9,14 +8,6 @@ export default defineConfig({
         },
     },
     plugins: [
-        viteStaticCopy({
-            targets: [
-                // {
-                //     src: 'node_modules/js-confetti/dist/js-confetti.min.js',
-                //     dest: 'js-confetti/js-confetti.min.js'
-                // }
-            ]
-        }),
         laravel({
             input: [
                 'resources/css/app.css',
