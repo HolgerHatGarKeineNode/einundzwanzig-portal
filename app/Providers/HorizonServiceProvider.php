b2606dcafc182f6aca134f122b4cf793846a74d3
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2025-11-24 00:40:06 +0100

    🔒 Update Horizon gate to use Nostr public keys instead of email for authorization

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,8 +28,10 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user = null) {
-            return in_array(optional($user)->email, [
-                //
+            return in_array(optional($user)->nostr, [
+                'npub1kz50hl2fk8rkkax3mv8kzx7vdhh3nmuegncsaj8r23w2a49nf3wsne6ejy',
+                'npub1pt0kw36ue3w2g4haxq3wgm6a2fhtptmzsjlc2j2vphtcgle72qesgpjyc6',
+                'npub1qwj482ffpvnwy4g7twejs4ckgnag9yxpjndqslk6juagmngwwhfsqfe5vq',
             ]);
         });
     }
