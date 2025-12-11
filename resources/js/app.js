7b0feca2986a2112015b2351ecd7cc983c1e1f2e
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2025-12-11 16:58:49 +0100

    🌍 Add German translations for Self-Hosted Services and related terms 🔄 Update community and service localization strings 📝 Include new UI labels and placeholders for service management features

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
 import nostrLogin from "./nostrLogin.js";
 
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+window.L = L;
+
 Alpine.data('nostrLogin', nostrLogin);
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,16 +13,7 @@
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet">
-
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-      crossorigin=""/>
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-        crossorigin=""></script>
+@googlefonts
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
