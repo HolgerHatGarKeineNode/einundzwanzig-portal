172217d3883d322170dc718966aa7a4779c9f7ab
BT <fsociety.mp4@pm.me>
2026-05-02 22:06:35 +0100

    🛠️ **Migration:** Added `hasTable` check to `meetup_user` migration to prevent redundant table creation.

--- database/migrations/2023_01_17_175957_create_meetup_user_table.php
+++ database/migrations/2023_01_17_175957_create_meetup_user_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('meetup_user')) {
+            return;
+        }
+
         Schema::disableForeignKeyConstraints();
 
         Schema::create('meetup_user', function (Blueprint $table) {
